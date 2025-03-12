04f3ce3edd381eec6b452e2aad582cfb6754d358
F1rdavs9644 <begmatovsamandar726@gmail.com>
2025-03-12 14:10:27 +0500

    here are Controller and Validations

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'age' => 'required|integer|min:18',
+            'password' => 'required|confirmed|min:8',
+            'product_name' => 'required|string|min:3',
+            'avatar' => 'image|mimes:jpeg,png,jpg|max:2048',
+            'website' => 'required|url',
+            'price' => 'required|numeric|min:0',
+            'published_at' => 'required|date',
+            'start_date' => 'required|date|after:today',
+            'end_date' => 'required|date|before:start_date',
+        ];
+    }
+}
